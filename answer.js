acc372dcfbf9c62f48900179796559d95dbe57a1
Marching <marching118@gmail.com>
2016-10-10 17:30:41 +0800

    server without db and answer

---
+++ answer.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const request = require('request-promise'),
+  Promise = require('bluebird');
+
+const mail = 'marching118@gmail.com';
+const host = 'localhost';
+let questionId;
+
+function buildLimitRequest(n) {
+  let pending = [];
+  let running = 0;
+
+  function run() {
+    let runPromise = request(...arguments);
+    runPromise.then(() => pending.length ? pending.shift()() : running--);
+    return runPromise;
+  }
+
+  return function () {
+    if (running >= n || pending.length) {
+      return new Promise(resolve => pending.push(resolve)).then(run.bind(null, ...arguments));
+    } else {
+      running++;
+      return run(...arguments);
+    }
+  };
+}
+const request5 = buildLimitRequest(5);
+
+request5({
+  method: 'POST',
+  url: `http://${host}:50000/1610/new-question`,
+  json: {mail}
+})
+  .then(r => {
+    console.log(r);
+    questionId = r.id;
+    const getTree = nodeId => request5(`http://${host}:50000/1610/questions/${questionId}/get-children/${nodeId}`, {json: true})
+      .then(children => Promise.all(children.map(getTree)))
+      .then(children => ({id: nodeId, children}));
+    return getTree(r.rootId);
+  })
+  .then(root => request5({
+    method: 'POST',
+    url: `http://${host}:50000/1610/questions/${questionId}/check`,
+    json: {root}
+  }))
+  .then(r => {
+    console.log(r);
+    if (r.pass) {
+      return request5({
+        method: 'POST',
+        url: `http://${host}:50000/1610/questions/${questionId}/submit`,
+        json: {
+          name: 'Ma Qin',
+          forFun: false,
+          phone: '15900000000',
+          sourceCode: require('fs').readFileSync('./answer.js', 'utf-8')
+        }
+      })
+    }
+  })
+  .then(r => console.log(r))
+  .catch(e => {
+    console.error(e);
+  });
---
+++ question.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const uuid = require('uuid'),
+  _ = require('lodash');
+
+const questions = {};
+
+function buildQuestion(ip, mail) {
+  console.log(`[NEW] ${mail} (${ip})`);
+
+  let nodeGroups = _.range(20)
+    .map(i => _.random(1, 4))
+    .reduce((r, n) => {
+      let nextId = _.last(_.last(r)) + 1;
+      return [...r, _.range(nextId, nextId + n)]
+    }, [[1]]);
+
+  const root = {
+    id: nodeGroups.shift()[0],
+    // depth: 1,
+    children: []
+  };
+  const nodeMap = {[root.id]: root};
+
+  let toAdd = [root];
+  while (nodeGroups.length) {
+    let newToAdd = [];
+    for (let node of toAdd) {
+      if (nodeGroups.length) {
+        node.children = nodeGroups.shift().map(id => ({
+          id,
+          // depth: node.depth + 1,
+          children: []
+        }));
+        newToAdd = newToAdd.concat(node.children);
+        node.children.forEach(n => nodeMap[n.id] = n)
+      }
+    }
+    toAdd = newToAdd;
+  }
+
+  // console.log(JSON.stringify(root, null, 2));
+  // Object.keys(nodeMap).forEach(id => console.log(id, nodeMap[id].children.map(i => i.id)));
+
+  const question = {
+    id: uuid.v4(),
+    ip,
+    mail,
+    root,
+    nodeMap,
+    c: 0,
+    cMax: 0,
+    startAt: Date.now(),
+    check: root => {
+      question.pass = _.isEqual(question.root, root);
+      question.endAt = Date.now();
+      if (!question.pass) {
+        delete question[question.id];
+        console.log(`[FAIL] ${mail} (${ip})`);
+      } else {
+        console.log(`[SUCCESS] ${mail} (${ip})`);
+      }
+      return question.pass;
+    },
+    submit: (data) => {
+      delete question[question.id];
+      console.log(`[SUBMIT] ${mail} (${ip})`);
+    }
+  };
+  questions[question.id] = question;
+
+  setTimeout(() => delete question[question.id], 5 * 60 * 1000);
+  return question;
+}
+
+function getQuestion(questionId) {
+  return questions[questionId];
+}
+
+module.exports = {
+  buildQuestion,
+  getQuestion
+};
